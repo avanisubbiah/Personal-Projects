201ac63b1a8028c400a5cbb1e89cc1277df49f33
avanishsubbiah <avanishsubbiah1@gmail.com>
2020-02-06 06:41:15 -1000

    Removed Redundant functions

--- PersonalProjects/AnswersText/LDCalc.py
+++ PersonalProjects/AnswersText/LDCalc.py
@@ -2,65 +2,15 @@
 from Levenshtein import distance
 from collections import OrderedDict
 
+
 # Class for Answer Reader
-
-
 class answerReader:
-    # Method to read real answers and return realADict
-    def realAReader():
-        # Reading realA.txt
-        with open("realA.txt") as realATxt:
-            realADict = OrderedDict()
-            # Reading all lines in realA.txt into keysAndVals
-            keysAndVals = realATxt.readlines()
-            i = 0
-            key = []  # Seperate array to store keys
-            val = []  # Seperate array to store values
-
-            # keysAndVals is seperated into key and vas
-            for value in keysAndVals:
-                keyVal1st = keysAndVals[i].split(":")
-                val1st = keyVal1st[1].split(";")
-                keyVal = []
-                keyVal.append(keyVal1st[0])
-                j = 0
-                for value in val1st:
-                    keyVal.append(val1st[j])
-                    j += 1
-                j = 0
-                key.append(keyVal[0].rsplit())
-                innerValList = []
-                j = 0
-                for value in keyVal:
-                    if(j != 0):
-                        innerValList.append(keyVal[j].rsplit())
-                    j += 1
-                val.append(innerValList)
-                i += 1
-            i = 0
-            # TODO Remove test prints when done
-            # print(key)
-            # print(val)
-            # For every value in key update dictionary with next key and value
-            for value in key:
-                keyNow = str(key[i])
-                realADict.update({keyNow: val[i]})
-                i += 1
-            # TODO Remove test print loop when done
-            '''
-            print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-            for keys,values in realADict.items():
-                print(keys)
-                print(realADict[keys])
-            '''
-        return realADict
-
-    # Method to read generated answers and return aDict
-    def aReader():
-        with open("A.txt") as aTxt:
+    # Method to read a text file: "txtName"
+    def reader(txtName):
+        with open(txtName) as txt:
             aDict = OrderedDict()
             # Reading all lines in A.txt into keysAndVals
-            keysAndVals = aTxt.readlines()
+            keysAndVals = txt.readlines()
             i = 0
             key = []  # Seperate array to store keys
             val = []  # Seperate array to store values
@@ -108,11 +58,10 @@ class levenshteinD:
     # Method to calculate the distance between val in dict
     def distanceCalc():
         # Initializing variables and calling reader
-        realADict = answerReader.realAReader()
-        aDict = answerReader.aReader()
+        realADict = answerReader.reader("realA.txt")
+        aDict = answerReader.reader("A.txt")
         distList = []
         avgDistDouble = None
-
         # Iterating and calculating distance for each key in realADict
         for key in realADict:
             # Flattening realADict values for this key into one list
