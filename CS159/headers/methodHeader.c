8b6b12668c1f77e4e37ea33fef87330ae93c6631
avanishsubbiah <avanishsubbiah1@gmail.com>
2020-02-13 09:26:55 -0500

    Added Lab04 Work

---
+++ CS159/headers/methodHeader.c
@@ -0,0 +1,16 @@
+/***************************************************************************
+*
+*  Function Information
+*
+*  Name of Function:
+*
+*  Function Return Type:
+*
+*  Parameters (list data type, name, and comment one per line):
+*    1.
+*    2.
+*    3.
+*
+*  Function Description:
+*
+***************************************************************************/
